be4d2d948d3eae2b2705dff3a4c155453b7c0da9
Winston  Chung <18.chung.winston@ctkprep.org>
2020-05-09 00:14:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ class Formatter {
   }
   
   static titleize(string) {
-    
+    return string.replace(
+            /\w\S*/g,
+            function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
   }
 }
